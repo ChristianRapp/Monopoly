5c08a94bd95ad2d5447669c42a2535c2ce9059ad
ChristianRapp <christian.rapp@hotmail.com>
2015-12-04 09:10:05 -0700

    Did stuff

--- Monopoly/src/Runner.java
+++ Monopoly/src/Runner.java
@@ -1,16 +1,15 @@
 import java.util.*;
 
-import javax.swing.plaf.synth.SynthSpinnerUI;
-
 public class Runner
 	{
 		static Scanner userInput = new Scanner(System.in);
+		static int playerCount;
 		static ArrayList<Square> board = new ArrayList<Square>();
 		static ArrayList<Player> players = new ArrayList<Player>();
 		static boolean isPlaying =true;
 		public static void main(String[] args)
 			{
-				addProperty();
+				
 				addPlayers();
 				while(isPlaying == true)
 				{
@@ -20,6 +19,30 @@ public class Runner
 				
 				
 			}
+		
+		public static void mainMenu()
+		{
+			System.out.println("Hello, welcome to monopoly");
+			System.out.println("How many players would you like?");
+			playerCount = userInput.nextInt();
+			System.out.println(" Would you like to play (1)regular monopoly or (2)Star Wars?");
+			int starWarsOrRegular = userInput.nextInt();
+			switch(starWarsOrRegular)
+			{
+				case 1:
+					{
+					addProperty();
+					}
+				case 2: 
+					{
+					addStarWarsProperty();		
+					}
+			}
+			
+			
+			
+			
+		}
 		
 		public static void playGame()
 		{
@@ -120,7 +143,7 @@ public class Runner
 			System.out.println();
 		}
 		
-		public static void addProperty()
+		public static void addStarWarsProperty()
 		{
 			board.add(new Go());
 			//Colored properties are buy price, rent, mortgage, color, name
@@ -148,30 +171,83 @@ public class Runner
 			board.add(new ColoredProperty(200, 16,  0, "Yavin 4", "Temple Throne Room"));
 			
 			board.add(new FreeParking("Free Parking"));
-			board.add(new ColoredProperty(60, 2,  0, "Cloud City", "Landing Platform"));
+			board.add(new ColoredProperty(220, 18,  0, "Cloud City", "Landing Platform"));
 			board.add(new CommunityChest("Community Chest 1"));
-			board.add(new ColoredProperty(60, 4,  0, "Cloud City", "Carbon Freezing Chamber"));
-			board.add(new ColoredProperty(60, 4,  0, "Cloud City", "Reactor Control Room"));
+			board.add(new ColoredProperty(220, 18,  0, "Cloud City", "Carbon Freezing Chamber"));
+			board.add(new ColoredProperty(240, 20,  0, "Cloud City", "Reactor Control Room"));
 			
 			board.add(new Railroad(200, 0,"X-Wing Fighter"));
-			board.add(new ColoredProperty(100, 6,  0, "Death Star", "Landing Bay"));
-			board.add(new ColoredProperty(120, 8,  0, "Death Star", "Throne Room"));
+			board.add(new ColoredProperty(260, 22,  0, "Death Star", "Landing Bay"));
+			board.add(new ColoredProperty(260, 22,  0, "Death Star", "Throne Room"));
 			board.add(new Utility(150, 0, "Moisture Farm"));
-			board.add(new ColoredProperty(100, 6,  0, "Death Star", "Central Core"));
+			board.add(new ColoredProperty(280, 22,  0, "Death Star", "Central Core"));
 			
 			board.add(new GoToJail("Go To Jail"));
-			board.add(new ColoredProperty(140, 10,  0, "Endor", "Forest"));
-			board.add(new ColoredProperty(160, 12,  0, "Endor", "Shield Generator"));
+			board.add(new ColoredProperty(300, 26,  0, "Endor", "Forest"));
+			board.add(new ColoredProperty(300, 26,  0, "Endor", "Shield Generator"));
 			board.add(new Chance("Chance 2"));
-			board.add(new ColoredProperty(140, 10,  0, "Endor", "Ewok Village"));
+			board.add(new ColoredProperty(320, 28,  0, "Endor", "Ewok Village"));
 			
 			board.add(new Railroad(200, 0,"Star Destroyer"));
 			board.add(new CommunityChest("Community Chest 1"));;
-			board.add(new ColoredProperty(180, 14,  0, "Coruscant", "Monument Square"));
-			board.add(new Tax("Bounty", 25));
-			board.add(new ColoredProperty(200, 16,  0, "Coruscant", "Imperial Palace"));
+			board.add(new ColoredProperty(350, 35,  0, "Coruscant", "Monument Square"));
+			board.add(new Tax("Bounty", 100));
+			board.add(new ColoredProperty(400, 50,  0, "Coruscant", "Imperial Palace"));
 				
 		}
+		
+		public static void addProperty()
+			{
+				board.add(new Go());
+				//Colored properties are buy price, rent, mortgage, color, name
+				board.add(new ColoredProperty(60, 2,  0, "Brown", "Mediterranean Avenue"));
+				board.add(new CommunityChest("Community Chest 1"));
+				board.add(new ColoredProperty(60, 4,  0, "Brown", "Baltic Avenue"));
+				
+				board.add(new Tax("Income Tax", 200)); 
+				board.add(new Railroad(200, 0,"Reading Railroad"));
+				board.add(new ColoredProperty(100, 6,  0, "Light Blue", "Oriental Avenue"));
+				board.add(new Chance("Chance 1"));
+				board.add(new ColoredProperty(100, 6,  0, "Light Blue", "Vermont Avenue"));
+				board.add(new ColoredProperty(120, 8,  0, "Light Blue", " Connecticut Avenue"));
+				
+				board.add(new Jail("Jail"));
+				board.add(new ColoredProperty(140, 10,  0, "Purple", "St. Charles Place"));
+				board.add(new Utility(150, 0, "Electrice Company"));
+				board.add(new ColoredProperty(140, 10,  0, "Purple", "States Avenue"));
+				board.add(new ColoredProperty(160, 12,  0, "Purple", "Virginia Avenue"));
+				
+				board.add(new Railroad(200, 0,"Pennsylvania Railroad"));
+				board.add(new ColoredProperty(180, 14,  0, "Orange", "St. James Place"));
+				board.add(new CommunityChest("Community Chest 2"));
+				board.add(new ColoredProperty(180, 14,  0, "Orange", "Tennessee Avenue"));
+				board.add(new ColoredProperty(200, 16,  0, "Orange", "New York Avenue"));
+				
+				board.add(new FreeParking("Free Parking"));
+				board.add(new ColoredProperty(220, 18,  0, "Red", "Kentucky Avenue"));
+				board.add(new Chance("Chance 2"));
+				board.add(new ColoredProperty(220, 18,  0, "Red", "Indiana Avenue"));
+				board.add(new ColoredProperty(240, 20,  0, "Red", "Illinois Avenue"));
+				
+				board.add(new Railroad(200, 0,"B&O Railroad"));
+				board.add(new ColoredProperty(260, 22,  0, "Yellow", "Atlantic Avenue"));
+				board.add(new ColoredProperty(260, 22,  0, "Yellow", "Ventnor Avenue"));
+				board.add(new Utility(150, 0, "Water Works"));
+				board.add(new ColoredProperty(280, 22,  0, "Yellow", "Marving Gardens"));
+				
+				board.add(new GoToJail("Go To Jail"));
+				board.add(new ColoredProperty(300, 26,  0, "Green", "Pacific Avenue"));
+				board.add(new ColoredProperty(300, 26,  0, "Green", "North Carolina Avenue"));
+				board.add(new Chance("Chance 3"));
+				board.add(new ColoredProperty(320, 28,  0, "Green", "Pennsylvania Avenue"));
+				
+				board.add(new Railroad(200, 0,"Short Line"));
+				board.add(new CommunityChest("Community Chest 4"));;
+				board.add(new ColoredProperty(350, 35,  0, "Blue", "Park Place"));
+				board.add(new Tax("Luxury Tax", 100));
+				board.add(new ColoredProperty(400, 50,  0, "Blue", "Boardwalk"));
+					
+			}
 		
 		public static void addPlayers()
 		{
